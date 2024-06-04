6fb02918f36c0530d2796e707daaa637f0edb121
Fraser Langton <fraser.langton@rendernetworks.com>
2024-06-04 20:17:48 +1000

    ia4

--- app/main.py
+++ app/main.py
@@ -6,10 +6,16 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()
+
+    # respond to requests
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.close()
 
 
 if __name__ == "__main__":
